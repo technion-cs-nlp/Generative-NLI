446a7d5e4dbd352abbb4136b6e8eb6d92446d734
Asael Dimion <dimion@rishon.csf.technion.ac.il>
2020-07-23 16:30:03 +0300

    updated script name

--- py-sbatch.sh
+++ py-sbatch.sh
@@ -28,7 +28,7 @@
 NUM_NODES=1
 NUM_CORES=2
 NUM_GPUS=1
-JOB_NAME="test_job"
+JOB_NAME="Premise-generator"
 MAIL_USER="dimion@campus.technion.ac.il"
 MAIL_TYPE=ALL # Valid values are NONE, BEGIN, END, FAIL, REQUEUE, ALL
 
