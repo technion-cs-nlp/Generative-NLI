6801129cecd5899b4c9110b38b955d0bf0e79bfe
Asael Dimion <dimion@lambda.csf.technion.ac.il>
2020-11-17 10:03:42 +0200

    added file to run jupyter on server

---
+++ jupyter-lab.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+###
+# CS236781: Deep Learning
+# jupyter-lab.sh
+#
+# This script is intended to help you run jupyter lab on the course servers.
+#
+# Example usage:
+#
+# To run on the gateway machine (limited resources, no GPU):
+# ./jupyter-lab.sh
+#
+# To run on a compute node:
+# srun -c 2 --gres=gpu:1 --pty jupyter-lab.shs
+#
+
+###
+# Conda parameters
+#
+CONDA_HOME=$HOME/miniconda3
+CONDA_ENV=base
+
+unset XDG_RUNTIME_DIR
+source $CONDA_HOME/etc/profile.d/conda.sh
+conda activate $CONDA_ENV
+
+jupyter lab --no-browser --ip=$(hostname -I) --port-retries=100
+
